d6100ca0782f318cd274eb57ad9a5183fb4e42d4
Abhishek6416 <abhishekkr6416@gmail.com>
2023-08-06 19:53:27 +0530

    backend

---
+++ spotdraft backend/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+// Replace these with your actual API keys
+const ASANA_ACCESS_TOKEN = 'YOUR_ASANA_ACCESS_TOKEN';
+const AIRTABLE_API_KEY = 'YOUR_AIRTABLE_API_KEY';
+const AIRTABLE_BASE_ID = 'YOUR_AIRTABLE_BASE_ID';
+
+app.use(express.json());
+
+// Endpoint to receive new task creation from Asana
+app.post('/asana/webhook', async (req, res) => {
+  try {
+    const asanaTask = req.body;
+    console.log('Received new Asana task:', asanaTask.name);
+
+    // Copy the Asana task to Airtable
+    await copyTaskToAirtable(asanaTask);
+
+    res.status(200).json({ message: 'Task copied to Airtable successfully.' });
+  } catch (error) {
+    console.error('Error while copying task to Airtable:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Function to copy a task from Asana to Airtable
+async function copyTaskToAirtable(asanaTask) {
+  const task = {
+    'Task ID': asanaTask.gid,
+    'Name': asanaTask.name,
+    'Assignee': asanaTask.assignee?.name || null,
+    'Due Date': asanaTask.due_on || null,
+    'Description': asanaTask.notes || null,
+  };
+
+  const airtableEndpoint = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/Asana%20Tasks`;
+  const airtableHeaders = {
+    'Authorization': `Bearer ${AIRTABLE_API_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    await axios.post(airtableEndpoint, { fields: task }, { headers: airtableHeaders });
+    console.log('Task copied to Airtable:', asanaTask.name);
+  } catch (error) {
+    console.error('Error while copying task to Airtable:', error.message);
+    throw error;
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
